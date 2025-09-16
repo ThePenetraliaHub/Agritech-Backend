2c14d004eb5d3a9dac2589fb0bc63256d6ca21fb
Oseghale Ehichioya Israel <ehichioya.oseghale@skilldrive.org>
2025-09-16 10:40:45 +0100

    updated pipeline

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("./config");
-const app_1 = require("./app");
-const PORT = config_1.config.PORT;
-app_1.app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
